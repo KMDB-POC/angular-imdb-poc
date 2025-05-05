008921907a38e46928f510d083fd051349406acb
lvakhoa <lvakhoa2304@gmail.com>
2025-05-05 13:00:55 +0700

    feat: add environment

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  apiBaseUrl: 'https://localhost:8081/api',
+  supabaseBaseUrl: 'https://mzkaxbegzabfjzjjjfmn.supabase.co/functions/v1',
+  supabaseAccessToken: '',
+};
